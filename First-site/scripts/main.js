90dfec3ea1751e97a73a90aa201bf5d6078f828d
yamakasivan <yamakasivan@mail.ru>
2022-08-16 12:57:47 +0800

    Work

--- First-site/scripts/main.js
+++ First-site/scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
